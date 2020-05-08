3e9d8e8ace39ee4d6ab72f9a4e6f084269bcf1be
sabrinarosecarroll <sabrinarosecarroll@gmail.com>
2020-05-08 16:48:10 -0400

    corrections to remoteControl_bare.py

--- py_scripts/remoteControl_bare.py
+++ py_scripts/remoteControl_bare.py
@@ -7,10 +7,10 @@ from sensor_msgs.msg import Joy
 class joyListenerNode():
 
     def __init__(self):
-        rp.init_node("my_joy_listener"")
+        rp.init_node("my_joy_listener")
 
         # Set up Subscriber
-        self.joy_sub = rp.Subscriber("/joy_node", Joy, self.joy_callback, queue_size=1)
+        self.joy_sub = rp.Subscriber("/joy", Joy, self.joy_callback, queue_size=1)
 
         rp.spin()
 
