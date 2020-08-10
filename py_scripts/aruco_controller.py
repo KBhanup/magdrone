28f4367a708f58a5199dce77d523142cdf169848
MikeK4y <mike.kalaitzakis@gmail.com>
2020-08-10 16:52:34 -0400

    Aruco controller fixes

--- py_scripts/aruco_controller.py
+++ py_scripts/aruco_controller.py
@@ -14,7 +14,8 @@ from pid import PIDcontroller
 from sensor_msgs.msg import Range
 from geometry_msgs.msg import Twist
 
-def to_quaternion(roll = 0.0, pitch = 0.0, yaw = 0.0):
+
+def to_quaternion(roll=0.0, pitch=0.0, yaw=0.0):
     """
     Convert degrees to quaternions
     """
@@ -32,16 +33,17 @@ def to_quaternion(roll = 0.0, pitch = 0.0, yaw = 0.0):
 
     return [w, x, y, z]
 
+
 class magdroneControlNode():
 
     def __init__(self):
         rp.init_node("magdrone_node")
 
-	# Create PID Controller
-	self.pid_z = PIDcontroller(0.1, 0.0, 0.0, 1)
+        # Create PID Controller
+        self.pid_z = PIDcontroller(0.1, 0.0, 0.0, 1)
 
-	# Create log file
-	self.log_file = open("log.txt", 'a');
+        # Create log file
+        self.log_file = open("log.txt", 'a')
 
         # Set up Subscribers
         self.pose_sub = rp.Subscriber("/aruco_single/pose", Range, self.pose_callback, queue_size=1)
@@ -57,6 +59,7 @@ class magdroneControlNode():
         self.land = 0
         self.dsrm = 0
         self.arm = 0
+        self.exit = 0
 
         # Create thread for publisher
         self.rate = 20
@@ -66,11 +69,11 @@ class magdroneControlNode():
         rp.spin()
 
     def printAndLog(self, msg):
-	print(msg)
-	self.log_file.write(msg)
-	self.log_file.write("\n")
+        print(msg)
+        self.log_file.write(msg)
+        self.log_file.write("\n")
 
-    def arm_and_takeoff_nogps(self, aTargetAltitude = -1.0):
+    def arm_and_takeoff_nogps(self, aTargetAltitude=-1.0):
         """
         Arms vehicle and fly to aTargetAltitude without GPS data.
         """
@@ -83,13 +86,13 @@ class magdroneControlNode():
         # Don't let the user try to arm until autopilot is ready
         # If you need to disable the arming check,
         # just comment it with your own responsibility.
-        #while not self.vehicle.is_armable:
-         #   print(" Waiting for vehicle to initialise...")
-          #  time.sleep(1)
+        # while not self.vehicle.is_armable:
+        #   print(" Waiting for vehicle to initialise...")
+        #  time.sleep(1)
 
         self.printAndLog("Arming motors")
         #  GUIDED_NOGPS mode is recommended by DroneKit
-        self.vehicle.mode = VehicleMode("GUIDED_NOGPS") 
+        self.vehicle.mode = VehicleMode("GUIDED_NOGPS")
         self.vehicle.armed = True
 
         while not self.vehicle.armed:
@@ -106,18 +109,19 @@ class magdroneControlNode():
             while True:
                 current_altitude = self.vehicle.location.global_relative_frame.alt
                 print(" Altitude: %f  Desired: %f" %
-                    (current_altitude, aTargetAltitude))
-                if current_altitude >= aTargetAltitude*0.95: # Trigger just below target alt.
+                      (current_altitude, aTargetAltitude))
+                # Trigger just below target alt.
+                if current_altitude >= aTargetAltitude*0.95:
                     print("Reached target altitude")
                     break
                 elif current_altitude >= aTargetAltitude*0.6:
                     thrust = SMOOTH_TAKEOFF_THRUST
-                self.set_attitude(thrust = thrust)
+                self.set_attitude(thrust=thrust)
                 time.sleep(0.2)
 
-    def send_attitude_target(self, roll_angle = 0.0, pitch_angle = 0.0,
-                             yaw_angle = None, yaw_rate = 0.0, use_yaw_rate = False,
-                             thrust = 0.5):
+    def send_attitude_target(self, roll_angle=0.0, pitch_angle=0.0,
+                             yaw_angle=None, yaw_rate=0.0, use_yaw_rate=False,
+                             thrust=0.5):
         """
         use_yaw_rate: the yaw can be controlled using yaw_angle OR yaw_rate.
                       When one is used, the other is ignored by Ardupilot.
@@ -132,21 +136,21 @@ class magdroneControlNode():
         # Thrust == 0.5: Hold the altitude
         # Thrust <  0.5: Descend
         msg = self.vehicle.message_factory.set_attitude_target_encode(
-            0, # time_boot_ms
-            1, # Target system
-            1, # Target component
+            0,  # time_boot_ms
+            1,  # Target system
+            1,  # Target component
             0b00000000 if use_yaw_rate else 0b00000100,
-            to_quaternion(roll_angle, pitch_angle, yaw_angle), # Quaternion
-            0, # Body roll rate in radian
-            0, # Body pitch rate in radian
-            math.radians(yaw_rate), # Body yaw rate in radian/second
+            to_quaternion(roll_angle, pitch_angle, yaw_angle),  # Quaternion
+            0,  # Body roll rate in radian
+            0,  # Body pitch rate in radian
+            math.radians(yaw_rate),  # Body yaw rate in radian/second
             thrust  # Thrust
         )
         self.vehicle.send_mavlink(msg)
 
-    def set_attitude(self, roll_angle = 0.0, pitch_angle = 0.0,
-                     yaw_angle = None, yaw_rate = 0.0, use_yaw_rate = False,
-                     thrust = 0, duration = 0.05):
+    def set_attitude(self, roll_angle=0.0, pitch_angle=0.0,
+                     yaw_angle=None, yaw_rate=0.0, use_yaw_rate=False,
+                     thrust=0, duration=0.05):
         """
         Note that from AC3.3 the message should be re-sent more often than every
         second, as an ATTITUDE_TARGET order has a timeout of 1s.
@@ -155,73 +159,68 @@ class magdroneControlNode():
         Sending the message multiple times is the recommended way.
         """
         self.send_attitude_target(roll_angle, pitch_angle,
-                             yaw_angle, yaw_rate, use_yaw_rate,
-                             thrust)
+                                  yaw_angle, yaw_rate, use_yaw_rate,
+                                  thrust)
         start = time.time()
         while time.time() - start < duration:
             self.send_attitude_target(roll_angle, pitch_angle,
-                                 yaw_angle, yaw_rate, use_yaw_rate,
-                                 thrust)
+                                      yaw_angle, yaw_rate, use_yaw_rate,
+                                      thrust)
             time.sleep(0.1)
         # Reset attitude, or it will persist for 1s more due to the timeout
         self.send_attitude_target(0, 0, 0, 0, True, thrust)
-	msg = "            actual roll: " + str(roll_angle) + " actual pitch: " + str(pitch_angle)
-	self.printAndLog(msg)
+        msg = "            actual roll: " + str(roll_angle) + " actual pitch: " + str(pitch_angle)
+        self.printAndLog(msg)
 
     def pose_callback(self, data):
         self.cmds = Twist()
-	
-	#Create Empty Commands
-        self.cmds.linear.x  = 0 #roll
-        self.cmds.linear.y  = 0 #pitch
-        self.cmds.linear.z  = 0 #thrust
-        self.cmds.angular.z = 0 #yaw
 
-	#Define Tag Pose
-	tag_pose_z = data.pose.position.y[0] #actual z 
-	msg = "tag position: " + str(tag_pose_z)
-	self.printAndLog(msg)
+        # Create Empty Commands
+        self.cmds.linear.x = 0   # roll
+        self.cmds.linear.y = 0   # pitch
+        self.cmds.linear.z = 0   # thrust
+        self.cmds.angular.z = 0  # yaw
 
-    def PID(self):
+        # Define Tag Pose
+        tag_pose_z = data.pose.position.y[0]  # actual z
+        msg = "tag position: " + str(tag_pose_z)
+        self.printAndLog(msg)
 
-	#Define desired position
-	des_z = 0 
+        # Define desired position
+        des_z = 0
 
-	#Calculate Error
-	error_z = des_z - tag_pose_z
-	msg = "z error: " + str(error_z)
-	self.printAndLog(msg)	
+        # Calculate Error
+        error_z = des_z - tag_pose_z
+        msg = "z error: " + str(error_z)
+        self.printAndLog(msg)
 
-	#PID update error
-	pid_z.updateError(error_z)
+        # PID update error
+        self.pid_z.updateError(error_z)
 
-	#generate thrust command
-	self.cmds.linear.z = pid_z.getCommand()
-	msg = "thrust: " + str(self.cmds.linear.z)
-	self.printAndLog(msg)
+        # generate thrust command
+        self.cmds.linear.z = self.pid_z.getCommand()
+        msg = "thrust: " + str(self.cmds.linear.z)
+        self.printAndLog(msg)
 
     def send_commands(self):
         self.printAndLog("Accepting Commands")
-	self.PID() 
-	msg = "tag position: " + str(tag_pose_z) + "z error: " + str(error_z) + "thrust: " + str(self.cmds.linear.z)
-	self.printAndLog(msg)
 
         r = rp.Rate(self.rate)
         while not rp.is_shutdown():
-            #print(self.vehicle.attitude.yaw)
-            if self.cmds is not None:               
-                self.set_attitude(thrust = self.cmds.linear.z)
-		msg = "thrust: " + str(self.cmds.linear.z)
-		self.printAndLog(msg)
+            # print(self.vehicle.attitude.yaw)
+            if self.cmds is not None:
+                self.set_attitude(thrust=self.cmds.linear.z)
+                msg = "thrust: " + str(self.cmds.linear.z)
+                self.printAndLog(msg)
                 if self.dsrm > 0:
-		    self.printAndLog("Disarming")
-                    self.set_attitude(thrust = 0, duration = 8)
-		    self.printAndLog("Disarm complete")
+                    self.printAndLog("Disarming")
+                    self.set_attitude(thrust=0, duration=8)
+                    self.printAndLog("Disarm complete")
                 if self.arm > 0:
                     self.printAndLog("Arming...")
                     self.arm_and_takeoff_nogps()
-		if self.exit > 0:
-		    self.printAndLog("Switched to manual controls")
+                if self.exit > 0:
+                    self.printAndLog("Switched to manual controls")
             r.sleep()
 
 # Start Node
